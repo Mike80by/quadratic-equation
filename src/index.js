b1dc34a64f8929a7bc6cc597041a56c1422bfd00
mike80by@gmail.com <your_email@whatever.com>
2018-09-13 11:51:22 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let arr = [];
+  equation = equation.split(' ');
+  let a = +equation[0];
+  let b = +(equation[3] + equation[4]);
+  let c = +(equation[7] + equation[8]);
+  let discrim = b*b - 4*a*c;
+  arr[0] = Math.round((-b + Math.sqrt(discrim)) / (2*a));
+  arr[1] = Math.round((-b - Math.sqrt(discrim)) / (2*a));
+  return arr.sort((a, b) => a - b);
 }
